b40423e2b476c34cbcc0d848c7b016f15de0a655
Mustafa Vural <116458648+vural-mustafa@users.noreply.github.com>
2023-08-05 01:55:30 +0300

    PHP ile Kurs Eğitim Sitesi Yapımı
    Php kullanarak html,css,js yardımıyla öğrenciler için ücretli kurs sistemi yapımı.

---
+++ baglanti.php
@@ -0,0 +1,14 @@
+<?php
+
+$vt_sunucu="localhost";
+$vt_kullanici="root";
+$vt_sifre="";
+$vt_adi="Bilgilendikçe";
+
+$baglan=mysqli_connect($vt_sunucu,$vt_kullanici,$vt_sifre,$vt_adi);
+if(!$baglan)
+{
+ die("Veritabanı bağlantı işlemi başarısız.".mysqli_connect_error());
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Anasayfa | Bilgilendikçe</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c20485228a.js" crossorigin="anonymous"></script>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+
+</head>
+
+<body>
+    <section id="menu">
+        <div id="logo"> Bilgilendikçe </div>
+        <nav>
+            <a href="#anasayfa"><i class="fas fa-house-user ikon"></i>Anasayfa</a>
+            <a href="#Hakkimizda"><i class="fas fa-info ikon"></i>Hakkımızda</a>
+            <a href="#egitimler"><i class="fas fa-graduation-cap ikon"></i>Eğitimler</a>
+            <a href="#ekip"><i class="fas fa-user-plus ikon"></i>Ekip</a>
+            <a href="#iletisimh3"><i class="fas fa-map ikon"></i>İletişim</a>
+
+        </nav>
+    </section>
+    <section id="anasayfa">
+        <div id="black"></div>
+        <div id="icerik">
+            <h2>Bilgilendikçe</h2>
+            <hr width=300 align=left>
+            <p>Siz de bu programlama yolculuğuna katılmak ve geleceğin teknolojilerini şekillendirenler arasında yer
+                almak için bize katılın. Hep birlikte, hayallerinizdeki projeleri hayata geçirelim ve dijital dünyanın
+                sınırlarını zorlayalım.</p>
+        </div>
+    </section>
+    <section id="Hakkimizda">
+        <h3>Hakkımızda</h3>
+        <div class="container">
+            <div id="sol">
+                <h5 id="h5sol">Biz, Programlama Dilleri Rehberi olarak, dünya genelindeki programlama meraklılarına en
+                    kaliteli ve güvenilir bilgi kaynağını sunmak için buradayız.</h5>
+            </div>
+            <div id="sağ">
+                <span>P</span>
+                <p id="psag">rogramlama, çağımızın en önemli becerilerinden biri olarak öne çıkıyor. Teknoloji hızla
+                    gelişirken, yazılımın gücüyle hayatımızı daha da kolaylaştırmak mümkün hale geliyor. Bizler, bu
+                    dijital çağda programlama dünyasının kapılarını size açmak ve teknolojiye olan merakınızı beslemek
+                    için buradayız.
+                <p>
+            </div>
+            <img src="resimler/img/her-bilgisayarda-olmasi-gereken-programlar.jpg" alt="" class="img-fluid mt100">
+            <p id="pson">Sizleri Programlama Dilleri Rehberi olarak ailemize katılmaya davet ediyoruz. Dijital dünyanın
+                sınırlarını zorlamak ve geleceğin teknolojilerini şekillendirmek için birlikte çalışalım. Programlamayı
+                öğrenme yolculuğunda size eşlik etmek ve ilham vermek için buradayız.
+
+                Tekrar hoş geldiniz ve birlikte harika işler başaralım!</p>
+        </div>
+    </section>
+    <section id="egitimler">
+        <div class="container">
+            <h3>Eğitimler</h3>
+            <div class="owl-carousel owl-theme">
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/html.jpg" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">HTML5 ve CSS3 Eğitimi</h5>
+                    <p class="cardp">HTML5, web sayfalarınızın yapısını belirlemenize olanak tanır. Başlıklardan
+                        paragraflara, resimlerden bağlantılara kadar her şeyi düzenlemenizi sağlar. CSS3 ise
+                        sayfalarınızın görünümünü şekillendirmenize olanak sağlar. Renkler, yazı stilleri, düzen ve
+                        animasyonlar gibi tasarım öğelerini kontrol etmenizi mümkün kılar.</p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/js.png" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">Javascript Eğitimi</h5>
+                    <p class="cardp">
+                        JavaScript, web geliştirme alanında oldukça önemli bir rol oynayan bir programlama dilidir. Web
+                        sayfalarını interaktif hale getirmek, dinamik içerikler oluşturmak ve kullanıcılarla etkileşim
+                        sağlamak için kullanılır. Eğitim kurumları, öğrencilere bu güçlü ve çok yönlü dilin temellerini
+                        ve ileri düzey konseptlerini öğretirken, modern web uygulamaları geliştirme süreçlerinde de
+                        etkin bir şekilde kullanımını teşvik ederler.
+                    </p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/PHP_Logo.png" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">PHP Eğitimi</h5>
+                    <p class="cardp">PHP, web geliştirme alanında oldukça popüler ve güçlü bir programlama dilidir. PHP, özellikle sunucu taraflı web uygulamaları geliştirmek için kullanılan açık kaynaklı bir dil olarak bilinir. Bu dil, HTML ile kolayca birleştirilebilen, veritabanlarıyla etkileşim sağlayabilen ve dinamik içerikler oluşturmayı sağlayan özellikleriyle tercih edilmektedir.
+
+                    </p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/asp.net-core-logo.png" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">ASP.Net Core Eğitimi</h5>
+                    <p class="cardp">ASP.NET Core, Microsoft tarafından geliştirilen açık kaynaklı ve platform bağımsız bir web uygulama çerçevesidir. Bu çerçeve, güçlü, yüksek performanslı ve ölçeklenebilir web uygulamaları oluşturmak için kullanılır. ASP.NET Core, .NET Core'un bir parçasıdır ve Windows, macOS ve Linux gibi farklı işletim sistemlerinde çalışabilir.
+                    </p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/nodejs.jpg" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">Node js Eğitimi</h5>
+                    <p class="cardp">Node.js kullanarak web sunucusu oluşturabilir, dosya işlemleri gerçekleştirebilir, veritabanı işlemleri yapabilir, API'lar oluşturabilir, gerçek zamanlı uygulamalar geliştirebilir ve daha pek çok farklı uygulama türünü hayata geçirebilirsiniz.
+                    </p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/react.png" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">React js Eğitimi</h5>
+                    <p class="cardp">React.js, Facebook tarafından geliştirilen ve kullanıcı arayüzü oluşturmak için kullanılan açık kaynaklı bir JavaScript kütüphanesidir. React, özellikle tek sayfalı uygulamalarda (Single Page Applications - SPA) etkili bir şekilde çalışır ve kullanıcı deneyimini geliştirmek için dinamik ve etkileşimli UI (Kullanıcı Arayüzü) oluşturmayı sağlar.
+
+
+                    </p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/c.png" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">C# Eğitimi</h5>
+                    <p class="cardp">C# (C Sharp), Microsoft tarafından geliştirilen, genel amaçlı, nesne tabanlı ve güçlü bir programlama dilidir. C#, Microsoft'un .NET platformu için ana programlama dilidir ve Windows uygulamaları, web uygulamaları, oyunlar, sunucu uygulamaları ve mobil uygulamalar gibi çeşitli alanlarda kullanılabilir.
+
+
+                    </p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/PYTHON.jpeg" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">Python Eğitimi</h5>
+                    <p class="cardp">Python, son derece popüler, yüksek seviyeli ve genel amaçlı bir programlama dilidir. Basit ve anlaşılır sözdizimi sayesinde öğrenmesi kolaydır ve bu nedenle hem acemi programcılar hem de deneyimli yazılım geliştiricileri tarafından tercih edilir. Python, çok çeşitli uygulamalarda kullanılabilir, örneğin web geliştirme, bilimsel hesaplamalar, veri analizi, yapay zeka, makine öğrenmesi, oyun geliştirme ve otomasyon gibi alanlarda yaygın olarak kullanılır.
+
+
+                    </p>
+                </div>
+                <div class="card item" data-merge=1.5>
+                    <div class="aspect-ratio"><img src="resimler/img/JAVA.png" alt="" class="img-fluid"></div>
+                    <h5 class="baslikcard">java Eğitimi</h5>
+                    <p class="cardp">Java kurs eğitimi, katılımcılara Java programlama dilini öğretmeyi ve Java ile çeşitli türde uygulamalar geliştirmeyi hedefleyen bir eğitim programıdır. Java, platform bağımsız, nesne yönelimli ve güçlü bir programlama dilidir. Hem masaüstü uygulamaları, hem de web uygulamaları, mobil uygulamalar ve büyük ölçekli sunucu uygulamaları gibi farklı alanlarda yaygın olarak kullanılır.
+
+
+                    </p>
+                </div>
+
+            </div>
+        </div>
+        <section id="ekip">
+            <div class="container"></div>
+            <h3 id="ekiph3">Çalışanlarımız</h3>
+            <div class="sutun-sol-sag">
+            <img src="resimler/img/ekip6.jpg" alt="" class="img-fluid oval">
+            <h4 class="ekipisim"> Mehmet Kaya</h4>
+            <p class="ekipp">Merhaba! <br>Ben HTML ve CSS hocanızım ve sizlere web geliştirmenin sihirli dünyasına adım atmanız için buradayım! Web siteleri oluşturmak ve tasarlamak isteyen herkes için bu heyecan dolu yolculuğa başlamak için doğru yerdesiniz.                HTML ve CSS, web geliştirmenin temel taşlarıdır. HTML ile sayfaların yapısal bileşenlerini oluştururken, CSS ile de bu sayfaların renk, font, düzen ve animasyon gibi görsel özelliklerini belirleyeceğiz. Birlikte, modern web tasarımının püf noktalarını ve en iyi uygulamaları keşfedeceğiz.</p>
+
+            <div class="ekip-ikon">
+                <a href="#"><i class="fab fa-facebook social"></i></a>
+                <a href="#"><i class="fab fa-instagram social"></i></a>
+                <a href="#"><i class="fab fa-twitter social"></i></a>
+            </div>
+            </div>
+            <div class="sutun">
+                <img src="resimler/img/ekip2.jpg" alt="" class="img-fluid oval">
+                <h4 class="ekipisim"> Şule Evirgen</h4>
+                <p class="ekipp">Merhaba!<br> Ben C# hocanızım ve size programlamayı öğrenmenin heyecanını yaşatmak için buradayım! C# (C Sharp), Microsoft tarafından geliştirilen güçlü ve çok yönlü bir programlama dilidir. Bu dille birlikte, bilgisayar dünyasına olanakların sınırlarını zorlayabileceğiniz bir pencere açıyoruz.</p>
+    
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fab fa-facebook social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-twitter social"></i></a>
+                </div>
+            </div>
+            <div class="sutun-sol-sag">
+                <img src="resimler/img/ekip3.jpg" alt="" class="img-fluid oval">
+                <h4 class="ekipisim"> Fatma Yıldız</h4>
+                <p class="ekipp">Merhaba!<br>Ben Java hocanızım ve sizlere programlamayı öğrenmenin en keyifli yollarından birini sunmak için buradayım! Java, geniş bir uygulama yelpazesine sahip, güçlü ve popüler bir programlama dilidir. Bu dil sayesinde gerçek dünyadaki problemleri çözecek uygulamalar geliştirmenin heyecanını yaşayacaksınız.
+                    </p>
+    
+                <div class="ekip-ikon">
+                    <a href="#"><i class="fab fa-facebook social"></i></a>
+                <a href="#"><i class="fab fa-instagram social"></i></a>
+                <a href="#"><i class="fab fa-twitter social"></i></a>
+                </div>
+            </div>
+        
+    </section>
+
+<section id="iletisim">
+    <div class="container"></div>
+            <h3 id="iletisimh3">İletişim</h3>
+           <form action="index.php" method="post">
+            <div id="iletisimopak">
+                <div id="formgroup">
+                    <div id="form-sol">
+                        <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                        <input type="tel"  name="Telefon_Numarasi" placeholder="Telefon Numarası" required class="form-control">
+                    </div>
+                    
+                    <div id="form-sag">
+                        <input type="email" name="email" placeholder="Email Adresiniz" required class="form-control">
+                        <input type="text"  name="Konu" placeholder="Konu Başlığı" required class="form-control">
+                    </div>
+                    <textarea name="mesaj" id="" cols="30" rows="10" placeholder="Mesaj Giriniz" required class="form-control"></textarea>
+                    <input type="submit" value="Gönder">
+                </div>
+                <div id="adres">
+                    <h4 id="adresbaslik">Adres:</h4>
+                     <p class="adresp">Bakkalbaşı Mah.</p>
+                     <p class="adresp">Çaldıran Sok. No:21</p>
+                     <p class="adresp">0272 440 00 00 </p>
+                     <p class="adresp">bilgilendikce@gmail.com</p>
+                </div>
+            </div>
+            </form>
+            <footer>
+                <div id="copyright">2023 |
+                Tüm Hakları Saklıdır.</div>
+                <div id="socialfooter">
+                    <a href="#"><i class="fab fa-facebook social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-twitter social"></i></a>
+                </div>
+                <a href="#menu"><i class="fas fa-angle-up" id="up"></i></a>
+            </footer>
+            
+        </div>
+</section>
+</section>
+    <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js"
+        integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
+    <script src="owl/owl.carousel.min.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
+
+
+<?php
+
+include("baglanti.php");
+
+if(isset($_POST["isim"],$_POST["Telefon_Numarasi"],$_POST["email"],$_POST["Konu"],$_POST["mesaj"]))
+{
+    $adsoyad=$_POST["isim"];
+    $tel=$_POST["Telefon_Numarasi"];
+    $email=$_POST["email"];
+    $konu=$_POST["Konu"];
+    $mesaj=$_POST["mesaj"];
+
+    $ekleme="INSERT INTO iletisim (id, adsoyad, telefon, email, konu, mesaj) VALUES ('$adsoyad','$tel','$email','$konu','$mesaj')";
+
+    if($baglan->query($ekleme)===TRUE)
+{
+    echo "<script>alert('Mesajiniz başarili bir şekilde gönderilmiştir.')</script>";
+}
+else
+{
+    echo "<script>alert('Mesajiniz gönderme işlemi başarisiz oldu.')</script>";
+}
+
+}
+?>
